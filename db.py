5f7a3ce877ce7b0c46b1218fe0dc5d2182524400
Varun Swaminathan <85042815+flerovious@users.noreply.github.com>
2023-11-16 01:46:14 -0800

    Add youtube transcript llama index reader

--- db.py
+++ db.py
@@ -1,23 +1,53 @@
 # youtube_processing.py
 
 from llama_index.readers import YoutubeTranscriptReader
-from llama_index.indices import GPTListIndex
+from llama_index import VectorStoreIndex
+from llama_index.query_engine import CitationQueryEngine
+from llama_index.response.schema import RESPONSE_TYPE
 
 
 # Function to process YouTube URL
-def process_youtube_url(youtube_url):
+def process_youtube_url(youtube_url: str):
     reader = YoutubeTranscriptReader(
         collection_name="youtube_collection",
-        persist_directory="data_connectors/youtube_collection"
+        persist_directory="data_connectors/youtube_collection",
     )
 
     documents = reader.load_data(ytlinks=[youtube_url])
-    index = GPTListIndex.from_documents(documents)
+    index = VectorStoreIndex.from_documents(documents)
 
-    return index.as_query_engine()
+    query_engine = CitationQueryEngine.from_args(
+        index,
+        similarity_top_k=3,
+        # here we can control how granular citation sources are, the default is 512
+        citation_chunk_size=512,
+    )
+
+    return query_engine
 
 
 # Function to handle search query
-def handle_search_query(query_engine, query_text):
+def handle_search_query(query_engine: CitationQueryEngine, query_text: str):
     response = query_engine.query(query_text)
     return response
+
+
+# Function to get answer
+def get_answer(response: RESPONSE_TYPE):
+    return response.response
+
+
+# Function to get text of top citations
+def get_top_citations(response: RESPONSE_TYPE):
+    citations = []
+    for source_node in response.source_nodes:
+        video_id = source_node.node.get_metadata_str().split()[-1]
+        text = response.source_nodes[0].node.get_text().replace("\n", " ")
+
+        citations.append(
+            {
+                "video_id": video_id,
+                "text": text,
+            }
+        )
+    return citations
